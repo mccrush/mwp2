cdc091a8c90ff8af8dadaafff38780e848c89c45
mccrush <mccrush2027@gmail.com>
2025-09-29 22:39:04 +0700

    Remove old code

--- src/shared/stores/projects.js
+++ src/shared/stores/projects.js
@@ -7,15 +7,6 @@ export const useProjectsStore = defineStore('projects', {
     }),
 
     actions: {
-        fetchProjects() {
-            // Simulate fetching projects from an API or database
-            this.projects = [
-                { id: 1, name: 'Project Alpha' },
-                { id: 2, name: 'Project Beta' },
-                { id: 3, name: 'Project Gamma' },
-            ]
-        },
-
         async getProjects(userId) {
             await getItems('projects', userId, '*').then(data => {
                 this.projects = data
@@ -25,7 +16,6 @@ export const useProjectsStore = defineStore('projects', {
         },
 
         async addProject(project, userId) {
-            //this.projects.push(project)
             await addItem('projects', project).then(() => {
                 this.getProjects(userId)
             }).catch(error => {
